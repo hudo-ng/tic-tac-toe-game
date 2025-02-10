a44b1e2d91e34ab3d7ac86e23f2c47af5d557a36
hudo-ng <hmdong901026@gmail.com>
2025-02-10 14:38:59 -0700

    decide winner functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,31 @@ import Button from "./components/button";
 
 const initialList = new Array(9).fill("");
 function App() {
-  const [value, setValue] = useState("X");
+  const [value, setValue] = useState("x");
   const [listOfValue, setListOfValue] = useState(initialList);
+  const [numMoveLeft, setNumMoveLeft] = useState(9);
+  const [hasWinner, setHasWinner] = useState(null);
 
   function handleUserClick(index) {
     const nextValue = value === "x" ? "o" : "x";
-    setValue(nextValue);
-    if (listOfValue[index] === "") {
-      listOfValue[index] = nextValue;
+    if (listOfValue[index] === "" && !hasWinner) {
+      const newDataList = [...listOfValue];
+      newDataList[index] = nextValue;
+      setListOfValue(newDataList);
+      setNumMoveLeft(numMoveLeft - 1);
+      setValue(nextValue);
+      const winner = checkWinner(newDataList);
+      console.log(winner);
+      if (winner) {
+        setHasWinner(winner);
+      }
     }
-    setListOfValue(listOfValue);
   }
   return (
     <div>
       <h1>Tic Tac Toe Game</h1>
       <div className="board">
-        {initialList.map((item, index) => (
+        {listOfValue.map((item, index) => (
           <Button
             value={item}
             key={index}
@@ -27,8 +36,29 @@ function App() {
           />
         ))}
       </div>
+      <div>{hasWinner ? <p>Winner is: {hasWinner}</p> : undefined}</div>
     </div>
   );
 }
 
 export default App;
+
+function checkWinner(data) {
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let line of winLines) {
+    const [a, b, c] = line;
+    if (data[a] && data[a] === data[b] && data[a] === data[c]) {
+      return data[a];
+    }
+  }
+  return null;
+}
